ded4a82bb78342b50c61ada95ae792245cb6c863
ukopoju <Matti.luuk1@gmail.com>
2023-11-29 12:59:34 +0200

    price-update

--- src/Pages/Price-list-Page/Prices.tsx
+++ src/Pages/Price-list-Page/Prices.tsx
@@ -15,23 +15,86 @@ const Hinnat = () => {
         <div className='underline'></div>
       </header>
       <div className='Paikka'>
-      <h1 className='otsikko'>Jäsenyydet</h1>
+      <h1 className='otsikko'>JÄSENYYDET</h1>
       
       <div className="container">
   <div className="tuote">1kk</div>
   <div className="hinta1">40€</div>
 </div>
+<br></br>
 <div className="container">
   <div className="tuote">12kk</div>
   <div className="hinta2">400€</div>
 </div>
+<br></br>
 <div className="container">
-  <div className="tuote">päivä</div>
+  <div className="tuote">kertakäynti</div>
   <div className="hinta3">7€</div>
 </div>
+<br></br>
+<br></br>
 
+<div className='personaltrainer'>
+<h1 className='otsikko'>PERSONALTRAINER</h1>
 
-      
+<div className="container">
+  <div className="tuote">Tunti</div>
+  <div className="hinta2">50€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">4h</div>
+  <div className="PRhinta">175€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">6h</div>
+  <div className="PRhinta2">250€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">10h</div>
+  <div className="PRhinta3">425€</div>
+</div>
+<p className='PRp'>Kun ostat personaltrainer tunteja voit sopia valitsemasi trainerin
+   kassa päivät jolloin haluat hänen avustusta</p>
+   </div>
+   <div className='personaltrainer'>
+<h1 className='otsikko'>TREENIOHJELMA</h1>
+
+<div className="container">
+  <div className="tuote">HARDCORE DIET - <br></br>Ravinto- ja treeniohjelma
+</div>
+  <div className="TRhinta">39,90€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">GET SHREDDED - <br></br>treeniohjelma
+</div>
+  <div className="TRhinta2">39,90€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">GET HUGE - <br></br>treeniohjelma
+</div>
+  <div className="TRhinta3">39,90€</div>
+</div>
+</div>
+<div className='personaltrainer'>
+<h1 className='otsikko'>RAVITSEMUSOHJELMA</h1>
+
+<div className="container">
+  <div className="tuote">6 Kuukautta</div>
+  <div className="RMhinta">39,90€</div>
+</div>
+<br></br>
+<div className="container">
+  <div className="tuote">12 Kuukautta</div>
+  <div className="RMhinta2">39,90€</div>
+</div>
+<p className='RMp'>Ravitsemusohjelmaan sisältyy kolme tapaamista ja yksi koko kehonkoostumusmittaus per kuukausi
++ ruokapäiväkirja, analyysit ja seuranta</p>
+</div>
       </div>
       
     </div>
